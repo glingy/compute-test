ecd2013907110c22d86638df3a96429d742856bf
Gregory Ling <0b0100011101101100@gmail.com>
2021-08-06 15:55:00 -0700

    Added multisrc recommendation

---
+++ sources/blinky/main.c
@@ -0,0 +1,16 @@
+#include <samc21.h>
+
+#define PORTA PORT->Group[0]
+
+int main() {
+  PORTA.DIR.reg = PORT_PA12 | PORT_PA13 | PORT_PA14 | PORT_PA15;
+  PORTA.OUTSET.reg = PORT_PA12 | PORT_PA13 | PORT_PA15;
+  PORTA.OUTCLR.reg = PORT_PA14;
+
+  while (1) {
+    for (int i = 0; i < 0x80000; i++) {
+      __NOP();
+    }
+    PORTA.OUTTGL.reg = PORT_PA12;
+  }
+}
