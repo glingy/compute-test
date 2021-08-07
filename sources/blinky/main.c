124a5001366f49be1e9940b74774842b8dc1a327
glingy <0b0100011101101100@gmail.com>
2021-08-07 14:11:53 -0500

    Updated gitignore

--- sources/blinky/main.c
+++ sources/blinky/main.c
@@ -5,12 +5,16 @@
 int main() {
   PORTA.DIR.reg = PORT_PA12 | PORT_PA13 | PORT_PA14 | PORT_PA15;
   PORTA.OUTSET.reg = PORT_PA12 | PORT_PA13 | PORT_PA15;
-  PORTA.OUTCLR.reg = PORT_PA14;
+  PORTA.OUTSET.reg = PORT_PA14;
 
   while (1) {
-    for (int i = 0; i < 0x80000; i++) {
+    for (int i = 0; i < 0x40000; i++) {
       __NOP();
     }
     PORTA.OUTTGL.reg = PORT_PA12;
+    for (int i = 0; i < 0x40000; i++) {
+      __NOP();
+    }
+    PORTA.OUTTGL.reg = PORT_PA12 | PORT_PA15;
   }
 }
