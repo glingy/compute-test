184500e746244bdc9b289dd39af66b7ff146e65f
Gregory Ling <0b0100011101101100@gmail.com>
2021-09-15 20:45:05 -0500

    Updated defs.h

--- include/pl_defs.h
+++ include/pl_defs.h
@@ -3,6 +3,11 @@
 
 // The syntax for PORT->Group[0] is way too verbose and isn't as clear as PORTA
 #define PORTA PORT->Group[0]
+#define PORTB PORT->Group[1]
+#define PORTC PORT->Group[2]
+#define PORTD PORT->Group[3]
+
+
 // Shorthand for passing the length of a constant array using sizeof
 #define ARRAY(arr) arr, (sizeof(arr) / sizeof(arr[0]))
 
